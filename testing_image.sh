5746f90ec5ffc901f8dfb218d0d0c957868725e1
belajarqywok <belajarqywok@gmail.com>
2023-10-23 11:48:57 +0700

    update testing image

--- testing_image.sh
+++ testing_image.sh
@@ -47,9 +47,11 @@ sleep 5
 docker logs ml_service
 docker container ls
 
-echo "[-  Installing Ngrok  -]"
+echo "[-  Installing Ngrok & Tunneling  -]"
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
         sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
         echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
         sudo tee /etc/apt/sources.list.d/ngrok.list && \
         sudo apt update && sudo apt install -y ngrok
+
+ngrok http 80
