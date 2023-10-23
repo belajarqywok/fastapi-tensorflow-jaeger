4c06e8fecb207087d658a4363c005d20149510d3
belajarqywok <belajarqywok@gmail.com>
2023-10-23 08:58:35 +0700

    add deployment configuration for GCE, GAE, Cloud Run

--- deployment/development.dockerfile
+++ deployment/development.dockerfile
@@ -1,22 +1,16 @@
-FROM python:3.9
+FROM python:3.9-bullseye
 
 LABEL team="cloud-computing-team"
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-WORKDIR /etc/bangkit_capstone
+WORKDIR /etc/petdoc_backend_service
 
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
-RUN cat << EOF >> entrypoint.sh \
-    #!/bin/bash \
-    alembic upgrade head \
-    uvicorn --host 0.0.0.0 --port 3000 --workers 10 main:REST \
-EOF
+COPY linux.requirements.txt /etc/petdoc_backend_service/
+RUN python3 -m pip install --upgrade pip
+RUN pip3 install -r requirements.txt
+COPY . /etc/petdoc_backend_service/
 
-RUN chmod r+x entrypoint.sh
-COPY . .
-
-EXPOSE 3000
-CMD [ "/bin/bash", "-c", "./entrypoint.sh" ]
+EXPOSE 80
+CMD [ "/bin/bash", "-c", "alembic upgrade head && uvicorn --host 0.0.0.0 --port 80 --workers 10 main:REST" ]
--- src/configurations/databases/postgresql.py
+++ src/configurations/databases/postgresql.py
@@ -1,23 +1,43 @@
 import os
 from dotenv import load_dotenv
 
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine, engine, create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 
-# Create Engine
-engine: Engine = create_engine(
-    url = (
-        f'postgresql+psycopg2://' +
-        f'{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASS")}' +
-        f'@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/' +
-        f'{os.getenv("POSTGRES_DB")}'
-    )
-)
+# Postgres Engines Options
+class Engines:
+    def __init__(self) -> None:
+        # IP Engine
+        self.ip_engine: Engine = create_engine(
+            url = (
+                f'postgresql+psycopg2://' +
+                f'{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASS")}' +
+                f'@{os.getenv("POSTGRES_HOST")}:{os.getenv("POSTGRES_PORT")}/' +
+                f'{os.getenv("POSTGRES_DB")}'
+            )
+        )
+
+        # UNIX Socket Engine
+        self.unix_sock_engine: Engine = create_engine(
+            engine.url.URL.create(
+                drivername = "postgresql+psycopg2",
+                username = {os.getenv("POSTGRES_USER")},
+                password = {os.getenv("POSTGRES_PASS")},
+                database = {os.getenv("POSTGRES_DB")},
+                query = {
+                    "unix_sock": (
+                        f'{os.getenv("POSTGRES_HOST")}' + 
+                        f'/.s.PGSQL.{os.getenv("POSTGRES_PORT")}'
+                    )
+                }
+            )
+        )
 
 # Session
+engine: Engine = Engines().ip_engine
 Session: sessionmaker[Session] = sessionmaker(
     autoflush  = False,
     autocommit = False,
